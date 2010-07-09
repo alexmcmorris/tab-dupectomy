28d2d3670ee6be733a8ed01a6db1a477a2e9381b
alex.mcmorris <alex.mcmorris@054abfe1-c9a1-65f5-dfc2-32a3a2187b9a>
2010-07-09 18:56:23 +0000

    formatting

--- td.js
+++ td.js
@@ -3,46 +3,46 @@ chrome.browserAction.onClicked.addListener(closeDuplicateTabsInCurrentWindow);
 
 function closeDuplicateTabsInCurrentWindow()
 {
-	chrome.tabs.getAllInWindow(null, closeDuplicateTabs);
+   chrome.tabs.getAllInWindow(null, closeDuplicateTabs);
 }
 
 function closeDuplicateTabs(tabs)
 {
-	var dupeCloser = new DuplicateCloser();
-	for (var index in tabs)
-	{
-		dupeCloser.closeIfDuplicate(tabs[index]);
-	}	
+   var dupeCloser = new DuplicateCloser();
+   for (var index in tabs) 
+   {
+      dupeCloser.closeIfDuplicate(tabs[index]);
+   }
 }
 
 function DuplicateCloser()
 {
-	this.cache = new TabCache();
-	
-	this.closeIfDuplicate = function(tab)
-	{
-		if (this.cache.exists(tab))
-		{
-			chrome.tabs.remove(tab.id);
-		}
-		else
-		{
-			this.cache.remember(tab);
-		}		
-	}
+   this.cache = new TabCache();
+   
+   this.closeIfDuplicate = function(tab)
+   {
+      if (this.cache.exists(tab)) 
+      {
+         chrome.tabs.remove(tab.id);
+      }
+      else 
+      {
+         this.cache.remember(tab);
+      }
+   }
 }
 
 function TabCache()
 {
-	this.tabs = [];
-	
-	this.exists = function(tab)
-	{
-		return this.tabs[tab.url.toLowerCase()];	
-	}
-	
-	this.remember = function(tab)
-	{
-		this.tabs[tab.url.toLowerCase()] = tab;
-	}	
+   this.tabs = [];
+   
+   this.exists = function(tab)
+   {
+      return this.tabs[tab.url.toLowerCase()];
+   }
+   
+   this.remember = function(tab)
+   {
+      this.tabs[tab.url.toLowerCase()] = tab;
+   }
 }
