eaf2f611b88a46dff2a6f9d8815667e0441174c7
alex.mcmorris <alex.mcmorris@054abfe1-c9a1-65f5-dfc2-32a3a2187b9a>
2010-07-12 19:37:47 +0000

    add duplicate tab count as badge text

--- trunk/td.js
+++ trunk/td.js
@@ -1,9 +1,21 @@
 
 chrome.browserAction.onClicked.addListener(closeDuplicateTabsInCurrentWindow);
+chrome.tabs.onUpdated.addListener(countDuplicateSiblings);
+chrome.tabs.onRemoved.addListener(countDuplicateSiblings);
 
 function closeDuplicateTabsInCurrentWindow()
 {
    chrome.tabs.getAllInWindow(null, closeDuplicateTabs);
+}
+
+function closeDuplicateSiblings(tab)
+{
+   chrome.tabs.getAllInWindow(tab.windowId, closeDuplicateTabs)
+}
+
+function countDuplicateSiblings(tab)
+{
+   chrome.tabs.getAllInWindow(tab.windowId, countDuplicateTabs);
 }
 
 function closeDuplicateTabs(tabs)
@@ -13,6 +25,36 @@ function closeDuplicateTabs(tabs)
    {
       dupeCloser.closeIfDuplicate(tabs[index]);
    }
+   
+   showDuplicateCount(0);
+}
+
+function countDuplicateTabs(tabs)
+{
+   var dupeCounter = new DuplicateCounter();
+   for (var index in tabs) 
+   {
+      dupeCounter.countIfDuplicate(tabs[index]);
+   }
+ 
+    showDuplicateCount(dupeCounter.count);
+}
+
+function showDuplicateCount(value)
+{
+	function BadgeText(value)
+	{
+	   if (value == 0) 
+	   {
+	      this.text = "";
+	   }
+	   else 
+	   {
+	      this.text = value + '';
+	   }
+	}
+
+    chrome.browserAction.setBadgeText(new BadgeText(value));
 }
 
 function DuplicateCloser()
@@ -24,6 +66,24 @@ function DuplicateCloser()
       if (this.cache.exists(tab)) 
       {
          chrome.tabs.remove(tab.id);
+      }
+      else 
+      {
+         this.cache.remember(tab);
+      }
+   }
+}
+
+function DuplicateCounter()
+{
+   this.cache = new TabCache();
+   this.count = 0;
+   
+   this.countIfDuplicate = function(tab)
+   {
+      if (this.cache.exists(tab)) 
+      {
+         this.count += 1;
       }
       else 
       {
@@ -46,3 +106,4 @@ function TabCache()
       this.tabs[tab.url.toLowerCase()] = tab;
    }
 }
+
